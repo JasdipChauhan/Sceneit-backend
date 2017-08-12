6b8e92a6b7238bb8dd21dc3521080115fb15b442
JasdipC <jasdip.chauhan@gmail.com>
2017-08-12 13:42:32 -0400

    using jbuilder to structure json responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :v1 do
+  namespace :v1, defaults: { format: :json } do # all requests need to be in json format
     resources :posts
-    resources :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy]
+    # ^ singleton because only one session for the logged in user
   end
 end
