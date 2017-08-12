3198f4520b640dc218ecf0eb335bd3f7faea5144
JasdipC <jasdip.chauhan@gmail.com>
2017-08-12 18:53:51 -0400

    end to end user confirmation with mailer options configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   acts_as_token_authenticatable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
   has_many :posts
 end
